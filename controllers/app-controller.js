aec14f4f8b6ed62ae150678da07d1979e11ff679
Adam Verma <vermaadam@gmail.com>
2020-02-09 20:30:57 -0600

    Required npm packages and export-ready function

---
+++ controllers/app-controller.js
@@ -0,0 +1,11 @@
+// Gather dependencies
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// Require models
+const db = require('../models');
+
+module.exports = function (app) {
+  
+}
