8008ed47f6283cc3e914aed84614d9f7bb4bb2f8
Adam Verma <vermaadam@gmail.com>
2020-02-09 20:23:55 -0600

    Created initial schema in Note.js

---
+++ models/Note.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Save a reference to the Schema constructor function
+const Schema = mongoose.Schema
+
+// Create a new Schema object with constructor
+let NoteSchema = new Schema({
+    // give 'headline' its properties
+    title: {
+        type: String
+    },
+
+    body: {
+        type: String
+    }
+
+})
+
+// Create model from mongoose's model scheme
+let Note = mongoose.models("Note", NoteSchema);
+
+// Export the Note model 
+module.exports = Note;
