7eea4c258936affd541a29236744dc2f1f1abd40
Adam Verma <vermaadam@gmail.com>
2020-02-09 20:33:32 -0600

    Get Route for Obtaining Web Data

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -7,5 +7,36 @@ const cheerio = require('cheerio');
 const db = require('../models');
 
 module.exports = function (app) {
-  
+    // Create GET route for scraping website
+    app.get("/", function (req, res) {
+        // Grab the html body of webpage with axio
+        axios.get("http://google.com/").then(function (response) {
+            // Use cheerio to load and save metadata as a $ JQuery selector
+            let $ = cheerio.load(response.data);
+
+            // Save an empty result as object; 
+            const result = {};
+            // Add the text 
+
+            //Create a new Article using the 'result' object in database
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // Confirm that result has been created
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // Log the error
+                    console.log(err);
+                });
+            // Send an OK confirmation to the client
+            res.send('Successful Scraping - YAH YEET')
+        });
+    });
+
+
+    app.listen(PORT, function() {
+        console.log(`App is running on PORT ${PORT}!`)
+    })
+
+
 }
